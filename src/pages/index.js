70a0021b50c8ab0c49179840f1c76e5f0f482a2a
Jeanette Andrews <jeanette@Jeanettes-MacBook.local>
2021-03-03 20:03:24 -0800

    try again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -10,7 +11,8 @@ const Home = () => {
 
     return (
         <>
-            <Navbar toggle={toggle}/> 
+            <Sidebar isOpen={isOpen} toggle={toggle}/> 
+            <Navbar toggle={toggle}/>
         </>
     )
 }
