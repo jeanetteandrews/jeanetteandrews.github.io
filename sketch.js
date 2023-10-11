7a747211f5ea81845426f50b8a0bb1b49fa8949b
Jeanette Andrews <jeanette@Jeanettes-MacBook.local>
2023-10-11 02:58:18 -0700

    add stupid donuts

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let myFont;
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+    const d = new Date();
+    document.getElementById("time").innerHTML = d.toLocaleTimeString(navigator.language, {hour: 'numeric', minute:'2-digit'}).replace("AM", "am").replace("PM","pm");
+}
+
+function setup() {
+    createCanvas(1284, 2778, WEBGL);
+    noStroke();
+  }
+  
+function draw() {
+
+  let locX = mouseX - 650;
+  let locY = mouseY - 1425;
+      
+  ambientLight(60, 60, 60);
+  pointLight(255, 255, 255, locX, locY, 100);
+
+
+  translate(locX, locY);
+  push();
+  ambientLight(100);
+  directionalLight(255, 255, 255, -1, 1, -1);
+  ambientMaterial('#2548d2');
+  fill('#ff0000');
+  specularMaterial(255);
+  shininess(150);
+  rotateX(mouseY * 0.01);
+  rotateY(mouseX * 0.01);
+  torus(120, 80, 40, 30);
+  pop();
+}
+  
